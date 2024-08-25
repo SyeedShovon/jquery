ef85983d1dcdb84f07b930e9bf8b14a349d7eefd
Syeed Shovon <shovoncsepstu@gmail.com>
2024-08-25 17:36:01 +0600

    Learning jquery

---
+++ index.js
@@ -0,0 +1,13 @@
+// alert('alhamdillah');
+// document.querySelector("h1").innerHTML = "All okay";
+
+// $("h1").text("Second jquery");
+
+// $(".my-div h1").text("jQuery class done");
+
+// $("#p1").text("Id jQuery done");
+
+
+// $("#p1, h1").text("all in comma");
+
+$("#p1").html("<b>id BOLD</b>")
